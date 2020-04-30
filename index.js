6bfab98960407d539b73c66ee0a69067fbc8398d
isaac-3 <isaacgc0596@gmail.com>
2020-04-30 07:46:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 // Your code here
-mapToNegativize(sourceArray)(num => num * -1)
+let mapToNegativize = function(sourceArray){
+    let newarr = []
+    sourceArray.forEach(function(num){
+        newarr.push(num * -1)
+    })
+    return newarr
+}
+
+let mapToNoChange = function(sourceArray){
+    let newarr = []
+    sourceArray.forEach(function(num){
+        newarr.push(num)
+    })
+    return newarr
+}
+
+let mapToDouble = function(sourceArray){
+    let newarr = []
+    sourceArray.forEach(function(num){
+        newarr.push(num * 2)
+    })
+    return newarr
+}
+
+let mapToSquare = function(sourceArray){
+    let newarr = []
+    sourceArray.forEach(function(num){
+        newarr.push(num * num)
+    })
+    return newarr
+}
+
+
+let reduceToTotal = function(sourceArray, startingPoint = 0){
+    sourceArray.forEach(function(num){
+        startingPoint += num
+    })
+    return startingPoint
+}
+
+let reduceToAllTrue = function(sourceArray){
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (!sourceArray[i]){
+            return false
+        }
+      }
+      return true
+}
+
+let reduceToAnyTrue = function(sourceArray){
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (sourceArray[i]){
+            return true
+        }
+      }
+      return false
+}
+
